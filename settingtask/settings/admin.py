7f9a2b0a3565dd4be91142445387a67e92dd2b29
harishAppiness <103628306+harishAppiness@users.noreply.github.com>
2022-08-16 16:18:53 +0530

    Add files via upload

--- settingtask/settings/admin.py
+++ settingtask/settings/admin.py
@@ -5,19 +5,19 @@ from .models import ApplicationSettings
 
 
 # Register your models here.
-# admin.site.site_header = 'Admin Settings Dashboard'
+admin.site.site_header = 'Admin Settings Dashboard'
 
-# class ApplicationSettingsAdmin(admin.ModelAdmin):
-#     list_display = ('id','module','setting_name','setting_value','edit_item','delete_item')
-#     list_filter = ('created','modified')
+class ApplicationSettingsAdmin(admin.ModelAdmin):
+    list_display = ('id','module','setting_name','setting_value','edit_item','delete_item')
+    list_filter = ('created','modified')
 
-#     def edit_item(self, obj):
-#         return format_html(f'<form action="http://127.0.0.1:8000/admin/settings/applicationsettings/{obj.id}/change/"><input type="submit" value="edit" /></form>')
-#     def delete_item(self, obj):
-#         return format_html(f'<form action="http://127.0.0.1:8000/admin/settings/applicationsettings/{obj.id}/delete/"><input type="submit" value="delete" /></form>')
+    def edit_item(self, obj):
+        return format_html(f'<form action="http://127.0.0.1:8000/admin/settings/applicationsettings/{obj.id}/change/"><input type="submit" value="edit" /></form>')
+    def delete_item(self, obj):
+        return format_html(f'<form action="http://127.0.0.1:8000/admin/settings/applicationsettings/{obj.id}/delete/"><input type="submit" value="delete" /></form>')
 
-admin.site.register(ApplicationSettings)
+admin.site.register(ApplicationSettings, ApplicationSettingsAdmin)
 # admin.site.register(ApplicationSettingsAdmin)
-# admin.site.unregister(Group)
+admin.site.unregister(Group)
 
 
